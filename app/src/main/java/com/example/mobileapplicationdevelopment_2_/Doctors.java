73cd2d84319ebdfd9d4d1f6024805db98f4ab0e7
Olaabudahi <olaabudahi12@gmail.com>
2025-08-11 17:47:44 +0300

    Inisial push

--- app/src/main/java/com/example/mobileapplicationdevelopment_2_/Doctors.java
+++ app/src/main/java/com/example/mobileapplicationdevelopment_2_/Doctors.java
@@ -1,7 +1,28 @@
 package com.example.mobileapplicationdevelopment_2_;
 
 public class Doctors {
-  
-  private  int id;
-  private String name;
+    private  int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Doctors(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
+
